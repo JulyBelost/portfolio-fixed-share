47646b649cf802e954e372a4f8bf787ae3095f8c
JulyBelost <julybelost@gmail.com>
2018-06-14 00:32:06 +0300

    add Hurst exponent computation, load_data function, wrap algo into function. Add parameters loop with output of algotithms compare table

--- algorithm.R
+++ algorithm.R
@@ -1,63 +1,139 @@
-#########################################################################################
-# setClass('finamDate')
-# setAs("character","finamDate", 
-#       function(from) {date_str <- as.character(from)
-#                       good_str <- ifelse(nchar(date_str)==6, date_str, paste("0", date_str, sep=""))
-#                       factor(as.Date(good_str, "%d%m%y")) })
-# 
-# stocks = read.delim("finam_output.txt", 
-#                   sep = ",", 
-#                   col.names = c("ticker", "per", "date", "time",  
-#                                 "open", "high", "low", "close", "vol"),
-#                   colClasses = c("factor", "NULL", "finamDate", "NULL",
-#                                  "NULL", "NULL", "NULL", "numeric", "NULL"))
-# 
-# stocks = transform(stocks, 
-#             price_ratio=ave(close, ticker, FUN=function(y) c(1, tail(y, -1) / head(y, -1))))
-# 
-# #добавить матрицу с р-шками, где они считаются. добавить столбец с ними в stocks
-# stocks$trust_level <- rep(1)
-################################### DATA LOADING ########################################
+library(data.table)
+library(pracma)
+library(useful)
+library(rowr)
 
 
-companies <- levels(stocks$ticker)
+################################### DATA LOADING #########################################
+setClass('finamDate')
+setAs("character","finamDate",
+      function(from) {date_str <- as.character(from)
+                      good_str <- ifelse(nchar(date_str)==6, date_str, paste("0", date_str, sep=""))
+                      factor(as.Date(good_str, "%d%m%y")) })
 
-# Initial parameters
-T <- nlevels(stocks$date)      # number of steps
-
-X_t <- c()
-
-N <- nlevels(stocks$ticker)    # number of instruments
-
-#alpha = 1/t
-
-w_ <- c()                      # w^*
-w <- rep(1/N, N)               # w
-w_m <- rep(1/N, N)             # w^m
-
-
-
-# Algorithm
-for (t in 1:T){
-  day <- stocks$date[order(levels(stocks$date))[t]]
-
-  inputs = subset(stocks, subset = date == day)
-  p_t = inputs$trust_level
-  
-  w_ = (p_t*w)/sum(p_t*w)
-  
-  x_t = inputs$price_ratio
-  
-  X_t[t] <- sum(x_t*w_)
-
-  
-  # LOSS UPDATE
-  w_m = (w * (p_t*x_t + (1 - p_t)*X_t[t]))/X_t[t]
-
-  # MIXING(Fixed-Share) UPDATE
-  alpha = 1/t
-  w = alpha/N + (1 - alpha)*w_m
-  
+Hs = function(ts){
+  return(hurstexp(ts, display=FALSE)[1])
 }
 
-K = cumprod(X_t) # portfolio wealth
+
+load_data = function(){
+  finam_data = read.delim("test.txt",
+                    sep = ",",
+                    col.names = c("ticker", "per", "date", "time",
+                                  "open", "high", "low", "close", "vol"),
+                    colClasses = c("factor", "NULL", "finamDate", "character",
+                                   "numeric", "NULL", "NULL", "numeric", "NULL"))
+  
+  stocks_raw = rbindlist(lapply(split(finam_data, list(finam_data$ticker, finam_data$date), lex.order = TRUE), 
+                            function(x) data.frame(head(x,1)[,1:2], 
+                                                   price_ratio = tail(x$close, 1)/head(x$open, 1), 
+                                                   ts =I(list(rbind(x$close, x$open))) )))
+  
+  # tail's argument n = -(window-1)
+  stocks_raw = rbindlist(lapply(split(stocks_raw, stocks_raw$ticker), 
+                            FUN=function(df) {
+                              new_df = tail(subset(df, select = c(ticker, date, price_ratio)), -1)
+                              new_df$hurst = as.numeric(rollApply(df$ts, function(x) Hs(c(unlist(x))), 
+                                                        window=2,minimum=2, align='right'))
+                              new_df
+                              }))
+  
+  return(stocks_raw)
+}
+##########################################################################################
+
+
+# hurst exponent transformation into trust levels
+ht_to_pt = function(a, b, hurst){
+  xi = function(a){ c(0, a-0.1, a, a+0.1, 1)}
+  yi = function(b){ c(0, b, 0.5, 1-b, 1)}
+  plot(xi(a),yi(b))
+  
+  trust_levels = pchip(xi(a), yi(b), hurst)
+  return(trust_levels)
+}
+
+
+# portfolio fixed share algorithm for unreliable instruments
+run_portfolio_fs = function(stocks, alpha){
+  # Initial parameters
+  T <- nlevels(stocks$date)      # number of steps
+  N <- nlevels(stocks$ticker)    # number of instruments
+  
+  w_ <- c()                      # w^*
+  w <- rep(1/N, N)               # w
+  w_m <- rep(1/N, N)             # w^m
+  
+  X_t <- c()
+  
+  day_nums = order(levels(stocks$date))
+  
+  # Algorithm   ###TODO:check for missing values and maybe order of instruments of p_t, x_t
+  for (t in 1:T){
+    day <- stocks$date[day_nums[t]]
+    inputs = subset(stocks, subset = date == day)
+    
+    p_t = inputs$trust_level
+    
+    w_ = (p_t*w)/sum(p_t*w)
+    
+    x_t = inputs$price_ratio
+    
+    X_t[t] <- sum(x_t*w_)
+    
+    
+    # LOSS UPDATE
+    w_m = (w * (p_t*x_t + (1 - p_t)*X_t[t]))/X_t[t]
+    
+    # MIXING(Fixed-Share) UPDATE
+    #alpha = 1/t
+    
+    w = alpha/N + (1 - alpha)*w_m
+  }
+  
+  K = cumprod(X_t) # portfolio wealth
+  
+  return(K)
+}
+
+
+stocks = load_data()
+
+
+# algorithm parameters
+a = c(0.5, 0.7)
+b = c(0.1, 0.25)
+alpha = c(0.001,0.01,0.1,0.25,1)
+
+# portfolio wealth vector for Buy and Hold algorithm
+K_n = rowMeans(data.frame(lapply(split(stocks_raw$price_ratio, stocks_raw$ticker), cumprod)))
+                              
+# algorithms evaluation output
+res <- data.frame(matrix(ncol = 6, nrow = 0))
+names <- c("a","b","alpha", "profit", "BandH", "SR")
+colnames(res) <- names
+
+
+for(l in 1:length(alpha)){
+  for(i in 1:length(a)){
+    for(j in 1:length(b)){
+      stocks$trust_level = ht_to_pt(a[i],b[j], stocks$hurst)
+      
+      # portfolio wealth vector for Portfolio Fixed-Share for unreliable instruments algorithm
+      K = run_portfolio_fs(stocks, alpha[l])
+      
+      res[nrow(res) + 1,] = list(a[i], b[j], alpha[l], tail(K, n=1), tail(K_n, n=1), sum(K>K_n)/length(K))
+    }
+  }
+  stocks$trust_level = rep(1,nrow(stocks))
+  
+  # portfolio wealth vector for Singer Portfolio algorithm
+  K_z = run_portfolio_fs(stocks, alpha[l])
+  
+  res[nrow(res) + 1,] = list(1, 1, alpha[l], tail(K_z, n=1), tail(K_n, n=1), sum(K_z>K_n)/length(K_z)) 
+}
+
+# par(mfrow = c(1, 2))
+# plot(K, pch = "*")
+# points(K_z, pch = "*", col = "blue")
+# points(K_n, pch = "*", col = "red")
